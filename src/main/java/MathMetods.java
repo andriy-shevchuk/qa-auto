dfd2d973ce6062644ecf11c5ae3d4c249b208f49
andriy-shevchuk <a.shevchuk.ua@gmail.com>
2017-05-16 21:09:59 +0300

    first commit

---
+++ src/main/java/MathMetods.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Admin on 13.05.2017.
+ */
+public class MathMetods {
+
+    public static int CalculateSum(int a, int b) {
+        int sum = a+b;
+        return sum;
+    }
+}
---
+++ src/test/java/MathMetodsTest.java
@@ -0,0 +1,12 @@
+/**
+ * Createdfghg by Admin on 13.05.2017.
+ */
+
+public class MathMetodsTest {
+
+    public static void main(String[] args) {
+        int x = MathMetods.CalculateSum(8,5);
+        System.out.println(x);
+    }
+
+}
