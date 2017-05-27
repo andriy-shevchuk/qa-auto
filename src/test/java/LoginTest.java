64b65386fdf8cf0b474841dc782c6d007b9bee16
andriy-shevchuk <a.shevchuk.ua@gmail.com>
2017-05-27 10:57:45 +0300

    SoftAssert, implicitlyWait, pageLoadTimeout added

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,10 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
 import java.util.concurrent.TimeUnit;
-
-import static java.lang.Thread.sleep;
 
 /**
  * Created by Admin on 20.05.2017.
@@ -21,27 +18,14 @@ public class LoginTest {
         webDriver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
         webDriver.navigate().to("https://alerts.shotspotter.biz/");
 
-
         webDriver.findElement(By.xpath("//input[@type='email']")).sendKeys("denvert1@shotspotter.net");
         webDriver.findElement(By.xpath("//input[@type='password']")).sendKeys("Test123!");
         webDriver.findElement(By.xpath("//*[@class='button' and text()='GO']")).click();
-        try {
-            sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        System.out.println(webDriver.getCurrentUrl());
+
         SoftAssert assertion = new SoftAssert();
-        assertion.assertEquals(webDriver.getCurrentUrl(),"https://alerts.shotspotter.biz/main");
 
-        System.out.println(webDriver.findElement(By.xpath("//*[@class='settings']")).isDisplayed());
         assertion.assertTrue(webDriver.findElement(By.xpath("//*[@class='settings']")).isDisplayed());
-
-        try {
-            sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+        assertion.assertTrue(webDriver.getCurrentUrl().contains("https://alerts.shotspotter.biz/main"));
 
         webDriver.quit();
         assertion.assertAll();
@@ -54,15 +38,9 @@ public class LoginTest {
         webDriver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
         webDriver.navigate().to("https://alerts.shotspotter.biz/");
 
-
         webDriver.findElement(By.xpath("//input[@type='email']")).sendKeys("incorrectEmail");
         webDriver.findElement(By.xpath("//input[@type='password']")).sendKeys("IncorrectPassword");
         webDriver.findElement(By.xpath("//*[@class='button' and text()='GO']")).click();
-        try {
-            sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
         SoftAssert assertion = new SoftAssert();
         assertion.assertTrue(webDriver.findElement(By.xpath("//*[@class='invalid-credentials']")).isDisplayed());
